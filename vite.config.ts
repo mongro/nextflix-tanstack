1e7812ecf5ba8a36d3e536d3c12a8816db812b60
mongro <simon.gross04@web.de>
2026-07-04 15:27:33 +0200

    -added nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   server: {
@@ -10,5 +11,5 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
-  plugins: [tailwindcss(), tanstackStart(), viteReact()],
-})
+  plugins: [tailwindcss(), tanstackStart(), nitro(), viteReact()],
+});
