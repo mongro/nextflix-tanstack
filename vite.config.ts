dad5b803f7f08430838b191b9337165a621187e3
mongro <simon.gross04@web.de>
2026-07-08 21:55:45 +0200

    add nitro

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,5 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
-  plugins: [tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [tailwindcss(), tanstackStart(), nitro(), viteReact()],
 });
